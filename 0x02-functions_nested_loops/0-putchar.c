6477dc4941d21a139036de371d9f4043b9a7bd98
ahmadothman1981 <ahmadothman@outlook.com>
2022-07-06 06:19:33 -0700

    added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#main.h
+/**
+ *  * main - Check description
+ *   * Description: It prints the word followed by a new line.
+ *    * Return: 0.
+*/
+int main()
+{
+	char msg[] = "_putchar";
+   	int i;
+	for (i = 0; i < 9 ; i++)
+	{
+		_putchar(msg[i]);
+	}
+		_putchar('\n');
+
+	return (0);
+}
